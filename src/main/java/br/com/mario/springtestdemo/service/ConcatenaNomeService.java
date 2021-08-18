b15491c6af5704c5ab19d504c86484fff4b0a109
Mario Rabelo <mutrano@gmail.com>
2021-08-18 08:48:19 -0300

    Adiciona properties de teste

--- src/main/java/br/com/mario/springtestdemo/service/ConcatenaNomeService.java
+++ src/main/java/br/com/mario/springtestdemo/service/ConcatenaNomeService.java
@@ -2,18 +2,21 @@ package br.com.mario.springtestdemo.service;
 
 import br.com.mario.springtestdemo.integration.RandomNameClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConcatenaNomeService {
+
+    @Value("${propriedade}")
+    private String propriedade;
 
     @Autowired
     private RandomNameClient client;
 
     public String concatenar(String nome) {
         var nomeBuscado = client.buscaNome();
-        System.out.println(nome);
-        return nome +" "+ nomeBuscado;
+        return nome +" "+ nomeBuscado + " "+ propriedade;
 
     }
 }
--- src/test/java/br/com/mario/springtestdemo/controller/ConcatenaNomeControllerTest.java
+++ src/test/java/br/com/mario/springtestdemo/controller/ConcatenaNomeControllerTest.java
@@ -1,17 +1,39 @@
 package br.com.mario.springtestdemo.controller;
 
 import br.com.mario.springtestdemo.SpringtestDemoApplication;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 
 import static org.junit.jupiter.api.Assertions.*;
-@SpringBootTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@SpringBootTest    // sobe o contexto do spring
+@TestPropertySource("/application-test.properties") // aponta o properties de teste
 public class ConcatenaNomeControllerTest {
 
 
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setup(WebApplicationContext wac) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();   // faz o setup do mockMvc com o contexto inteiro
+    }
+
+    //esta sem mocks na integracao externa
     @Test
-    public void concatena() {
+    void deveRetornarNomeConcatenadoComPropriedade() throws Exception {
+
+
+        mockMvc.perform(get("/o"))
+                .andDo(print());
+
 
 
     }
